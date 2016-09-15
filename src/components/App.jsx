92b29e1442d0a36db197f2248cee7de23fcaba01
Rajan Singh <rajan1992@hotmail.com>
2016-09-15 11:46:57 -0400

    Changed to a react application
    Switched from angular to react. Used create-react-app cli and npn run
    eject to get the current configuration. Also added Materialize css and
    tested it with a simple nabber

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import NavigationHeader from './navigation-header';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavigationHeader />
+      </div>
+      );
+  }
+}
+
+export default App;
---
+++ src/components/navigation-header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class NavigationHeader extends Component {
+
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo center">Restaurant Reviewer</a>
+        </div>
+      </nav>
+      );
+  }
+}
+
+export default NavigationHeader;
